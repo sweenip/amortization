be2d282a9690aa9d71f4ee9d4a4b25236ff30afa
Sweeni Ponoth <sweeni@hotmail.com>
2024-05-02 20:53:31 -0500

    tests/unit_tests/test_001_loan_future.py : PASS, tests/unit_tests/test_002_loan_current.py : FAIL on last bucket.. ref ???.txt

--- amortization/schedule.py
+++ amortization/schedule.py
@@ -4,9 +4,8 @@ from amortization.amount import calculate_amortization_amount
 from amortization.enums import PaymentFrequency
 
 
-def amortization_schedule(
-    principal: float, interest_rate: float, period: int, payment_frequency: PaymentFrequency = PaymentFrequency.MONTHLY
-) -> Iterator[Tuple[int, float, float, float, float]]:
+def amortization_schedule(principal: float, interest_rate: float, period: int, 
+                          payment_frequency: PaymentFrequency = PaymentFrequency.MONTHLY, actual_payment: float=None) -> Iterator[Tuple[int, float, float, float, float]]:
     """
     Generates amortization schedule
 
@@ -14,16 +13,24 @@ def amortization_schedule(
     :param interest_rate: Interest rate per year
     :param period: Total number of periods
     :param payment_frequency: Payment frequency per year
+    :param actual_payment : actual payment in each period (e.g. if including extra payment towards principal)
     :return: Rows containing period, amount, interest, principal, balance, etc
     """
     amortization_amount = calculate_amortization_amount(principal, interest_rate, period, payment_frequency)
+    if (actual_payment is None):
+        payment = amortization_amount
+    elif (actual_payment >= amortization_amount):
+        payment = actual_payment
+    else:
+        raise NotImplementedError(f"actual_payment must be None or >= amortization amount, which is currently computed as {amortization_amount:,.0f}")
+    
     adjusted_interest = interest_rate / payment_frequency.value
     balance = principal
     for number in range(1, period + 1):
         interest = round(balance * adjusted_interest, 2)
         if number < period:
-            principal = amortization_amount - interest
+            principal = payment - interest
             balance -= principal
         else:
-            principal, amortization_amount, balance = balance, balance + interest, 0
-        yield number, amortization_amount, interest, principal, balance
+            principal, payment, balance = balance, balance + interest, 0
+        yield number, payment, interest, principal, balance
